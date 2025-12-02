f48f32a78ad8d7a1a85325d36033b480864e037d
Felipe Denardo <felipe.denardo@cws.digital>
2025-12-02 14:38:54 -0300

    feat: add AuthMiddleware and RespondUnauthorized helper

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 require (
 	github.com/gin-gonic/gin v1.11.0
 	github.com/go-playground/validator/v10 v10.28.0
+	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/google/uuid v1.6.0
 	gorm.io/gorm v1.31.1
 )
---
+++ pkg/middleware/auth.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	httphelpers "github.com/felipedenardo/chameleon-common/pkg/http"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"strings"
+)
+
+func AuthMiddleware(secretKey string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			httphelpers.RespondUnauthorized(c, "auth header is empty")
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return []byte(secretKey), nil
+		})
+
+		if err != nil || !token.Valid {
+			httphelpers.RespondUnauthorized(c, "Invalid or expired token")
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			if userID, ok := claims["sub"].(string); ok {
+				c.Set("userID", userID)
+			}
+			if role, ok := claims["role"].(string); ok {
+				c.Set("role", role)
+			}
+		}
+
+		c.Next()
+	}
+}
