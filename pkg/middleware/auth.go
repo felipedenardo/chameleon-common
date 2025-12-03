4d0a79b0ec80cba1a4446903e24f4d8792a7c4ea
Felipe Denardo <felipe.denardo@cws.digital>
2025-12-02 23:40:29 -0300

    implement black list token checker interface

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	httphelpers "github.com/felipedenardo/chameleon-common/pkg/http"
+	"github.com/felipedenardo/chameleon-common/pkg/security"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"strings"
@@ -9,7 +10,7 @@ import (
 
 const RawTokenKey = "rawTokenString"
 
-func AuthMiddleware(secretKey string) gin.HandlerFunc {
+func AuthMiddleware(secretKey string, blacklistTokenChecker security.BlacklistTokenChecker) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		authHeader := c.GetHeader("Authorization")
@@ -20,6 +21,18 @@ func AuthMiddleware(secretKey string) gin.HandlerFunc {
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		tokenUnverified, _, _ := new(jwt.Parser).ParseUnverified(tokenString, jwt.MapClaims{})
+		if claims, ok := tokenUnverified.Claims.(jwt.MapClaims); ok {
+			jti, _ := claims["jti"].(string)
+
+			isBlacklisted, err := blacklistTokenChecker.IsTokenBlacklisted(c.Request.Context(), jti)
+
+			if err != nil || isBlacklisted {
+				httphelpers.RespondUnauthorized(c, "Token revogado ou erro de segurança.")
+				return
+			}
+		}
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
